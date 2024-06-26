4b35ad1cda62236797f372db9fea4b07d520c6b9
saipavan4572 <saipavan4572@gmail.com>
2024-06-27 00:30:18 +0530

    terraform aws vpc

--- outputs.tf
+++ outputs.tf
@@ -29,6 +29,6 @@ output "database_subnet_group_id" {
 #   value = aws_db_subnet_group.default.name
 # }
 
-# output "igw_id" {
-#   value = aws_internet_gateway.gw.id
-# }
+output "igw_id" {
+  value = aws_internet_gateway.gw.id
+}
